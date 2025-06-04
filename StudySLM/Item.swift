99340afa3c602d31bd0ba2c31582222c892a90ba
Veerjyot Singh <103166939+VeerjyotSingh@users.noreply.github.com>
2025-06-04 20:35:21 +0530

    Initial Commit

---
+++ StudySLM/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StudySLM
+//
+//  Created by Veerjyot Singh on 04/06/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StudySLMTests/StudySLMTests.swift
@@ -0,0 +1,16 @@
+//
+//  StudySLMTests.swift
+//  StudySLMTests
+//
+//  Created by Veerjyot Singh on 04/06/25.
+//
+
+import Testing
+
+struct StudySLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
